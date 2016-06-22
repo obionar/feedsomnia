5fa60c1a54270149c6d1575de0647de69545bd20
obionar <obionar@ro.ru>
2016-06-22 08:50:39 +0300

    print results in gui

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from tkinter import *
+from tkinter import ttk
 import sqlite3
 import os
 
@@ -10,38 +11,55 @@ c = conn.cursor()
 # Functions
 def search_filter():
     c.execute('''SELECT * FROM Feeds WHERE Title LIKE '%{}%' ORDER BY Published ASC'''.format(search_box.get()))
-    os.system('clear')
-
-    print("\n******************** Search Results for: {} ********************\n".format(search_box.get()))
-
     for row in c.fetchall():
-        print("Title:", row[0])
-        print("Category:", row[1])
-        print("Link:", row[3])
-        print()
+        search_results.insert(END, row[0])
+        print(row[0])
+
+
+#    os.system('clear')
+#    print("\n******************** Search Results for: {} ********************\n".format(search_box.get()))
+#    for row in c.fetchall():
+#        print("Title:", row[0])
+#        print("Category:", row[1])
+#        print("Link:", row[3])
+#        print()
+def inserts():
+    for entry in get:
+        search_results.insert(END, row[0])
+
+
 
 # tkinter GUI
 root = Tk()
 root.wm_title("Feedsomnia")
 
 # **** Toolbar ****
-toolbar = Frame(root, bd=1, relief=SUNKEN)
+toolbar = ttk.Frame(root)
 #search_label = Label(toolbar, text="Filter: ")
 #search_label.pack(side=LEFT)
-search_box = Entry(toolbar)
+search_box = ttk.Entry(toolbar)
 search_box.pack(side=LEFT, padx=4, pady=4)
-search_button = Button(toolbar, text="Search", command=search_filter)
+search_button = ttk.Button(toolbar, text="Search", command=search_filter)
 search_button.pack(side=LEFT, padx=4, pady=4)
 #search_autorefresh = Checkbutton(toolbar, text="Autorefresh")
 #search_autorefresh.pack(side=LEFT, padx=4, pady=4)
-exit_button = Button(toolbar, text="Exit", command=root.quit)
+exit_button = ttk.Button(toolbar, text="Exit", command=root.quit)
 exit_button.pack(side=LEFT, padx=4, pady=4)
 toolbar.pack(side=TOP, fill=X)
-# Todo: Show me search results here!
 
 img = PhotoImage(file="insomnia.png")
 imglabel = Label(root, image=img)
 imglabel.pack(pady=10)
+
+# Todo: Show me search results here!
+scrollbar = ttk.Scrollbar(root)
+scrollbar.pack(side=RIGHT, fill=Y)
+
+search_results = Listbox(root)
+search_results.pack( fill=BOTH, expand=2)
+scrollbar.config(command=search_results.yview)
+
+
 
 # **** Statusbar ****
 status = Label(root, text="Preparing to do nothing!", bd=1, relief=SUNKEN, anchor=W)
