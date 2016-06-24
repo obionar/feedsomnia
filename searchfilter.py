bf57774e02033a81b6b43cbe846bf48e3567c49b
obionar <obionar@ro.ru>
2016-06-24 18:08:29 +0300

    gui update

--- searchfilter.py
+++ searchfilter.py
@@ -14,7 +14,7 @@ class Filter(Tk):
 		self.toolbar.pack(fill=X, padx=4, pady=4)
 		self.entry = ttk.Entry(self.toolbar)
 		self.entry.bind_all("<KeyPress>", self.on_button)
-		self.button = ttk.Button(self.toolbar, text="Exit", command=self.quit)
+		self.button = ttk.Button(self.toolbar, text="Refresh", command=self.on_button)
 		self.entry.pack(side=LEFT, padx=4, pady=4)
 		self.button.pack(side=LEFT, padx=4, pady=4)
 		
@@ -24,14 +24,21 @@ class Filter(Tk):
 		self.scrollbar.pack(side=RIGHT, fill=Y)
 		self.listbox.config(yscrollcommand=self.scrollbar.set)
 		self.scrollbar.config(command=self.listbox.yview)
+	
+	def refresh(self):
+		self.listbox.update()
 
-	def on_button(self, event):
+	def on_button(self, *event):
 		self.listbox.delete(0,END)
+		self.listbox.update()
 		c.execute('''SELECT * FROM Feeds WHERE Title LIKE '%{}%' ORDER BY Published DESC'''.format(self.entry.get()))
-		for row in c.fetchall():	
-			self.listbox.insert(END, row[0])
+		for row in c.fetchall():
+			listitem = "*  {} ( {} )".format(row[0], row[1])
+			self.listbox.insert(END, listitem)
 
 app = Filter()
+app.title("Feedsomnia")
+app.minsize(width=400, height=300)
 app.mainloop()
 
 
