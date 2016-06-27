13e705755846f9c459ac98282d9330b9ea5de796
obionar <obionar@ro.ru>
2016-06-27 10:12:45 +0300

    gui fixes

--- Feedsomnia_tk.py
+++ Feedsomnia_tk.py
@@ -16,10 +16,11 @@ c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS Feeds(item_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
 			Title TEXT, Category TEXT, Published TEXT UNIQUE, Link TEXT)''')
 
+
 class Feedsomnia(Tk):
 	def __init__(self):
 		Tk.__init__(self)
-
+		
 		# toolbar
 		self.toolbar = ttk.Frame(self)
 		self.toolbar.pack(fill=X, padx=4, pady=4)
@@ -37,19 +38,22 @@ class Feedsomnia(Tk):
 		self.button = ttk.Button(self.toolbar, text="Quit", command=self.quit)
 		self.button.pack(side=RIGHT, padx=4, pady=4)
 
+
+		# middle area
+		self.content = ttk.Frame(self)
+		self.content.pack(expand=YES, fill=BOTH)
+
 		# TreeView
-		self.tree = ttk.Treeview(self)
+		self.tree = ttk.Treeview(self.content)
 		self.tree.pack(side=LEFT, expand=YES, fill=BOTH)
-		self.tree["columns"]=("item_id", "title", "category","date")
+		self.tree["columns"]=("title", "category","date")
 
 		self.tree.column("#0", width=0, stretch=NO) #Hidden URL
-		self.tree.column("item_id", width=0, stretch=NO)
-		self.tree.column("title", width=500)
-		self.tree.column("category", width=180, stretch=NO)
+		self.tree.column("title", minwidth=300, width=400)
+		self.tree.column("category", width=170, stretch=NO)
 		self.tree.column("date", width=150, stretch=NO)
 
 		self.tree.heading("#0", text="")
-		self.tree.heading("item_id", text="ID")
 		self.tree.heading("title", text="Title")
 		self.tree.heading("category", text="Category")
 		self.tree.heading("date", text="Published")
@@ -58,10 +62,15 @@ class Feedsomnia(Tk):
 		self.tree.bind("<Double-1>", self.double_click)
 
 		# scrollbar
-		self.scrollbar = ttk.Scrollbar(self)
+		self.scrollbar = ttk.Scrollbar(self.content)
 		self.scrollbar.pack(side=RIGHT, fill=Y)
 		self.tree.config(yscrollcommand=self.scrollbar.set)
 		self.scrollbar.config(command=self.tree.yview)
+		
+		# statusbar
+		self.labelvar = StringVar()
+		self.statusbar = Label(self, textvariable = self.labelvar, bd=1, relief=SUNKEN, anchor=W, pady=4, padx=8)
+		self.statusbar.pack(side=BOTTOM, fill=X)
 
 	#double click on item
 	def double_click(self, event):
@@ -69,7 +78,8 @@ class Feedsomnia(Tk):
 		print("you clicked on - ", self.tree.item(item,"text"))
 		open_url = self.tree.item(item,"text")
 		webbrowser.open_new_tab(open_url)
-	
+
+	# update db
 	def update_db(self):
 		print("getting feeds.. ")
 		self.tree.delete(*self.tree.get_children())
@@ -82,7 +92,12 @@ class Feedsomnia(Tk):
 			#print(Title)
 			c.execute ('''INSERT OR IGNORE INTO Feeds(Title, Category, Published, Link) VALUES (?, ?, ?, ?)''', 
 				(Title, Category, Published, Link))
-			conn.commit()
+		c.execute('''SELECT * FROM Feeds''')
+		self.itemscount = len(c.fetchall())
+		print (self.itemscount)
+		self.labelvar.set("Currently {} Items in database".format(self.itemscount))
+		conn.commit()
+		
 		print("\n({}) Database updated \n".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
 		self.get_results()
 		self.after(600000, self.update_db) #auto update db every 10 minutes
@@ -94,12 +109,13 @@ class Feedsomnia(Tk):
 		if len(self.get_entry) > 0:
 			c.execute('''SELECT * FROM Feeds WHERE Title LIKE '%{}%' ORDER BY Published ASC'''.format(self.get_entry))
 			for row in c.fetchall():
-				self.tree.insert("" , 0, text=row[4], values=(row[0], row[1], row[2], row[3]))
+				self.tree.insert("" , 0, text=row[4], values=(' * ' + row[1], row[2], row[3]))
+			
 
 
 
 app = Feedsomnia()
 app.title("Feedsomnia v0.1")
-app.minsize(width=400, height=200)
+app.minsize(width=800, height=400)
 app.update_db() #get new feeds at startup
 app.mainloop()
